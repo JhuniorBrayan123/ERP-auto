fd0a7537ea9639b9db9ef67e64f6c0b4cd047c98
Jhunior Brayan Gutierrez <gutierrezmamanib26@gmail.com>
2026-07-20 14:09:01 -0500

    Completando targets y flujo de proveedores

--- tests/ClientesProveedores/proveedores/PR-05_exportacion/descargar-proveedores.spec.ts
+++ tests/ClientesProveedores/proveedores/PR-05_exportacion/descargar-proveedores.spec.ts
@@ -1,10 +1,9 @@
-import {test, expect} from '@fixtures/clientes-proveedores/proveedores.fixture';
+import {test} from '@fixtures/clientes-proveedores/proveedores.fixture';
 import {
     AbrirMenuDescargasProveedor,
     DescargarProveedoresFiltrados,
     DescargarTodosLosProveedores,
 } from '@screenplay/tasks/clientes-proveedores/proveedores/ExportarProveedor';
-import {BuscarProveedorEnListado} from '@screenplay/tasks/clientes-proveedores/proveedores/BuscarProveedor';
 import {CrearProveedor} from '@screenplay/tasks/clientes-proveedores/proveedores/CrearProveedor';
 import {EliminarProveedor} from '@screenplay/tasks/clientes-proveedores/proveedores/EliminarProveedor';
 import {generarProveedorDNI} from '@data/clientes-proveedores/proveedores.data';
@@ -13,11 +12,11 @@ import {ProveedoresTargets} from '@screenplay/targets/clientes-proveedores/Prove
 test.describe('PR-05 | Exportación de Proveedores', {tag: ['@proveedores', '@exportacion']}, () => {
 
     test('SC-01: Descargar proveedores filtrados @PR-05.1', async ({proveedorActor, page}) => {
-        
+
         const datos = generarProveedorDNI();
         await proveedorActor.realiza(CrearProveedor(datos));
-        
-        
+
+
         await ProveedoresTargets.btnFiltrosAvanzados(page).click();
         await ProveedoresTargets.filtroTipoDocumento(page).click();
         await ProveedoresTargets.opcionFiltroTipoDoc(page, 'DNI').click();
@@ -25,18 +24,15 @@ test.describe('PR-05 | Exportación de Proveedores', {tag: ['@proveedores', '@ex
         await proveedorActor.realiza(AbrirMenuDescargasProveedor());
         await proveedorActor.realiza(DescargarProveedoresFiltrados());
 
-        
+
         await ProveedoresTargets.btnBorrarFiltros(page).click();
 
-        
+
         await proveedorActor.realiza(EliminarProveedor(datos.numeroDocumento));
     });
 
     test('SC-02: Descargar todos los proveedores @PR-05.2', async ({proveedorActor, page}) => {
-        
-        await ProveedoresTargets.btnBorrarFiltros(page).click().catch(() => {});
 
-        
         await proveedorActor.realiza(AbrirMenuDescargasProveedor());
         await proveedorActor.realiza(DescargarTodosLosProveedores());
     });
