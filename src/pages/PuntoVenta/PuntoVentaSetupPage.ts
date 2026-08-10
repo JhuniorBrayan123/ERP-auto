9b7a336f2fa66611dcda6f8eda0e645717a17f03
Jhunior Brayan Gutierrez <gutierrezmamanib26@gmail.com>
2026-08-10 14:46:09 -0500

    Cambiando selector en PV datos adicionales

--- src/pages/PuntoVenta/PuntoVentaSetupPage.ts
+++ src/pages/PuntoVenta/PuntoVentaSetupPage.ts
@@ -241,7 +241,7 @@ export class PuntoVentaSetupPage {
         await inputNombre.fill(campo.nombre);
 
         
-        await this.page.getByText('Selecciona documentos').click();
+        await this.page.getByText('Selecciona comprobantes').click();
         await this.page.locator('.v-checkbox-default-label:visible').filter({hasText: /^Todos$/}).first().click();
         await this.page.locator('.v-multiselect-form-header .vector').click();
         await this.page.waitForTimeout(300);
@@ -258,7 +258,7 @@ export class PuntoVentaSetupPage {
         await inputNombre.fill(campo.nombre);
 
         
-        await this.page.getByText('Selecciona documentos').click();
+        await this.page.getByText('Selecciona comprobantes').click();
         await this.page.locator('.v-checkbox-default-label:visible').filter({hasText: /^Todos$/}).first().click();
         await this.page.locator('.v-multiselect-form-header .vector').click();
         await this.page.waitForTimeout(300);
