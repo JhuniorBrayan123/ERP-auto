2a325ebc9c7427d34f08dac1bdf822235114a9c6
Jhunior Brayan Gutierrez <gutierrezmamanib26@gmail.com>
2026-09-07 18:20:30 -0500

    Cambiando la funcion para recibir el parametro de string

--- tests/ClientesProveedores/clientes/CL-02_validacion/validar-campos-obligatorios.spec.ts
+++ tests/ClientesProveedores/clientes/CL-02_validacion/validar-campos-obligatorios.spec.ts
@@ -1,14 +1,14 @@
-import {test, expect} from '@fixtures/clientes-proveedores/clientes.fixture';
+import {test} from '@fixtures/clientes-proveedores/clientes.fixture';
 import {
     AbrirCrearCliente,
-    SeleccionarTipoDocCliente,
-    LlenarNumeroDocumentoCliente,
-    LlenarNombreRazonSocialCliente,
     CambiarCodigoAManual,
-    LlenarDireccionCliente,
-    LlenarTelefonoCliente,
-    LlenarEmailCliente,
     ClickCrearCliente,
+    LlenarDireccionCliente,
+    LlenarEmailCliente,
+    LlenarNombreRazonSocialCliente,
+    LlenarNumeroDocumentoCliente,
+    LlenarTelefonoCliente,
+    SeleccionarTipoDocCliente,
 } from '@screenplay/tasks/clientes-proveedores/clientes/CrearCliente';
 import {
     CuerpoContieneTexto,
@@ -18,7 +18,7 @@ import {
 test.describe('CL-02 | Validación de Campos Obligatorios', {tag: ['@clientes', '@validacion']}, () => {
 
     test('SC-01: Validar campo código obligatorio @CL-02.1', async ({cliente}) => {
-        
+
         await cliente.realiza(AbrirCrearCliente());
         await cliente.realiza(SeleccionarTipoDocCliente('RUC'));
         await cliente.realiza(LlenarNumeroDocumentoCliente('10101010101'));
@@ -28,16 +28,16 @@ test.describe('CL-02 | Validación de Campos Obligatorios', {tag: ['@clientes', 
         await cliente.realiza(LlenarTelefonoCliente('999999999'));
         await cliente.realiza(LlenarEmailCliente('srqapruebaserp2@gmail.com'));
 
-        
+
         await cliente.realiza(ClickCrearCliente());
 
-        
-        await cliente.realiza(CuerpoContieneTexto('Campo obligatorio'));
+
+        await cliente.pregunta(CuerpoContieneTexto('Campo obligatorio'));
         await cliente.realiza(FormularioSigueMostrado());
     });
 
     test('SC-02: Validar campo nombre/razón social obligatorio @CL-02.2', async ({cliente}) => {
-        
+
         await cliente.realiza(AbrirCrearCliente());
         await cliente.realiza(SeleccionarTipoDocCliente('RUC'));
         await cliente.realiza(LlenarNumeroDocumentoCliente('10101010101'));
@@ -46,16 +46,16 @@ test.describe('CL-02 | Validación de Campos Obligatorios', {tag: ['@clientes', 
         await cliente.realiza(LlenarTelefonoCliente('999999999'));
         await cliente.realiza(LlenarEmailCliente('srqapruebaserp2@gmail.com'));
 
-        
+
         await cliente.realiza(ClickCrearCliente());
 
-        
-        await cliente.realiza(CuerpoContieneTexto('Campo obligatorio'));
+
+        await cliente.pregunta(CuerpoContieneTexto('Campo obligatorio'));
         await cliente.realiza(FormularioSigueMostrado());
     });
 
     test('SC-03: Validar campo número documento obligatorio @CL-02.3', async ({cliente}) => {
-        
+
         await cliente.realiza(AbrirCrearCliente());
         await cliente.realiza(SeleccionarTipoDocCliente('RUC'));
         await cliente.realiza(LlenarNombreRazonSocialCliente('automatizado-ruc-cliente'));
@@ -64,11 +64,11 @@ test.describe('CL-02 | Validación de Campos Obligatorios', {tag: ['@clientes', 
         await cliente.realiza(LlenarTelefonoCliente('999999999'));
         await cliente.realiza(LlenarEmailCliente('srqapruebaserp2@gmail.com'));
 
-        
+
         await cliente.realiza(ClickCrearCliente());
 
-        
-        await cliente.realiza(CuerpoContieneTexto('Campo obligatorio'));
+
+        await cliente.pregunta(CuerpoContieneTexto('Campo obligatorio'));
         await cliente.realiza(FormularioSigueMostrado());
     });
 });
